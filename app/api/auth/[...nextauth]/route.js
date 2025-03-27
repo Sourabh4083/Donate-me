7058a932a31602543a5534aa63763a40a53c0d41
Sourabh Prasad <72189816+Sourabh4083@users.noreply.github.com>
2025-03-27 16:43:32 +0530

    adding google provider in route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,8 @@
 import NextAuth from 'next-auth'
 // import AppleProvider from 'next-auth/providers/apple'
 // import FacebookProvider from 'next-auth/providers/facebook'
-// import GoogleProvider from 'next-auth/providers/google'
 // import EmailProvider from 'next-auth/providers/email'
+import GoogleProvider from 'next-auth/providers/google'
 import GitHubProvider from "next-auth/providers/github";
 import mongoose from "mongoose";
 import connectDb from '@/db/connectDb';
@@ -25,16 +25,17 @@ export const authoptions =  NextAuth({
     //     clientId: process.env.FACEBOOK_ID,
     //     clientSecret: process.env.FACEBOOK_SECRET
     //   }),
-    //   GoogleProvider({
-    //     clientId: process.env.GOOGLE_ID,
-    //     clientSecret: process.env.GOOGLE_SECRET
-    //   }),
+      GoogleProvider({
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET
+      }),
     //   // Passwordless / email sign in
     //   EmailProvider({
     //     server: process.env.MAIL_SERVER,
     //     from: 'NextAuth.js <no-reply@example.com>'
     //   }),
     ],
+  secrets: process.env.SECRET,
     callbacks: {
       async signIn({ user, account, profile, email, credentials }) {
          if(account.provider == "github") { 
